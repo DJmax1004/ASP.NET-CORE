47185ab519cf359f3e804832782ebdbee53093c8
Logon Aluno <logonaluno@local.com>
2019-08-19 11:57:58 -0300

    Exercicio Aula

---
+++ 03-FiapWebAspNet/03-FiapWebAspNet/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _03_FiapWebAspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _03_FiapWebAspNet.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return View(_lista);
+        }
+        private static IList<Usuario> _lista = new List<Usuario>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            ViewBag.usuario = usuario;
+            ViewData["nome"] = usuario.nome;
+            TempData["msg"] = "Usuário cadastrado com sucesso!";
+            //Cadastrar no "banco" de dados
+            _lista.Add(usuario);
+            //Envia o objeto usuário para a tela
+            return View(usuario);
+        }
+    }
+}
---
+++ 03-FiapWebAspNet/03-FiapWebAspNet/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _03_FiapWebAspNet.Models
+{
+    public class Usuario
+    {
+        public String nome { get; set; }
+        public DateTime dtNascimento { get; set; }
+        public String email { get; set; }
+    }
+}
