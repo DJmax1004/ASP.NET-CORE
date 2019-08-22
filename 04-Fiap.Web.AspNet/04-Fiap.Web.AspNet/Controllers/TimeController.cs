56391f78c4ce3b6b6356475d77d122eeda32b5d5
Logon Aluno <logonaluno@local.com>
2019-08-22 09:59:18 -0300

    Redirect

---
+++ 04-Fiap.Web.AspNet/04-Fiap.Web.AspNet/Controllers/TimeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _04_Fiap.Web.AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _04_Fiap.Web.AspNet.Controllers
+{
+    public class TimeController : Controller
+    {
+        private static IList<Time> _Lista = new List<Time>();
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return View(_Lista);
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(Time time)
+        {
+            TempData["msg"] = "Time cadastrado com sucesso!!!";
+            _Lista.Add(time);
+            return RedirectToAction("Listar");
+        }
+
+    }
+}
---
+++ 04-Fiap.Web.AspNet/04-Fiap.Web.AspNet/Models/Time.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _04_Fiap.Web.AspNet.Models
+{
+    public class Time
+    {
+        [Display(Name ="Nome do time")]
+        public String Nome { get; set; }
+        public bool Classificado { get; set; }
+        [Display(Name = "Vitórias")]
+        public int Vitorias { get; set; }
+        [Display(Name = "Data de fundação")]
+        [DataType(DataType.Date)]
+        public DateTime DataFundacao { get; set; }
+        [Display(Name = "Folha de pagamento")]
+        public decimal FolhaPagamento { get; set; }
+    }
+}
