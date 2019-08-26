9b5e618659bc23481fea7053f6846b1e1ad82601
Logon Aluno <logonaluno@local.com>
2019-08-26 11:40:08 -0300

    Enun e TagHelper

---
+++ 05-Fiap.Web.AspNet/05-Fiap.Web.AspNet/Controllers/ZooController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _05_Fiap.Web.AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _05_Fiap.Web.AspNet.Controllers
+{
+    public class ZooController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        private IActionResult Listar()
+        {
+            return View(_Lista);
+        }
+
+        private static IList<Zoo> _Lista = new List<Zoo>();
+
+        [HttpPost]
+        public IActionResult Cadastrar(Zoo zoo)
+        {
+            TempData["msg"] = "Zoologico cadastrado com sucesso!";
+            _Lista.Add(zoo);
+            return RedirectToAction("Listar");
+        }
+    }
+}
---
+++ 05-Fiap.Web.AspNet/05-Fiap.Web.AspNet/Models/Zoo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _05_Fiap.Web.AspNet.Models
+{
+    public class Zoo
+    {
+        public int Jaulas { get; set; }
+        public DateTime DataAbertura { get; set; }
+        public String Nome { get; set; }
+        public Tipo EntidadeAdministradora { get; set; }
+        
+    }
+
+}
